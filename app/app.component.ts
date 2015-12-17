d99d21260c6d909f80e1a660a4e07aee77684e9c
Eduard Y. Lepner <elepner@dataeast.com>
2015-12-17 14:06:45 +0600

    Created a frame for application with docking panels.

--- app/app.component.ts
+++ app/app.component.ts
@@ -9,7 +9,6 @@ export class AppComponent {
 
     constructor() {
 
-        console.log("Hello3");
         var divDockManager = document.getElementById("my_dock_manager");
         var dockManager = new dockspawn.DockManager(divDockManager);
         dockManager.initialize();
@@ -24,24 +23,18 @@ export class AppComponent {
         // They can then be docked on to the dock manager
         // Panels get a titlebar and a close button, and can also be
         // converted to a floating dialog box which can be dragged / resized
-        var solution = new dockspawn.PanelContainer(document.getElementById("solution_window"), dockManager);
+        var hypernetWindow = new dockspawn.PanelContainer(document.getElementById("hypernet_window"), dockManager);
         var properties = new dockspawn.PanelContainer(document.getElementById("properties_window"), dockManager);
-        var toolbox = new dockspawn.PanelContainer(document.getElementById("toolbox_window"), dockManager);
         var outline = new dockspawn.PanelContainer(document.getElementById("outline_window"), dockManager);
-        var problems = new dockspawn.PanelContainer(document.getElementById("problems_window"), dockManager);
-        var output = new dockspawn.PanelContainer(document.getElementById("output_window"), dockManager);
         var editor1 = new dockspawn.PanelContainer(document.getElementById("editor1_window"), dockManager);
         var editor2 = new dockspawn.PanelContainer(document.getElementById("editor2_window"), dockManager);
-        var infovis = new dockspawn.PanelContainer(document.getElementById("infovis"), dockManager);
+        
 
         // Dock the panels on the dock manager
         var documentNode = dockManager.context.model.documentManagerNode;
         var outlineNode = dockManager.dockLeft(documentNode, outline, 0.15);
-        var solutionNode = dockManager.dockFill(outlineNode, solution);
+        var solutionNode = dockManager.dockFill(outlineNode, hypernetWindow);
         var propertiesNode = dockManager.dockDown(outlineNode, properties, 0.6);
-        var outputNode = dockManager.dockDown(documentNode, output, 0.2);
-        var problemsNode = dockManager.dockRight(outputNode, problems, 0.40);
-        var toolboxNode = dockManager.dockRight(documentNode, toolbox, 0.20);
         var editor1Node = dockManager.dockFill(documentNode, editor1);
         var editor2Node = dockManager.dockFill(documentNode, editor2);
     }
